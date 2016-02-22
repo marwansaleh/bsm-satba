8bb5564080874c55e3dc662aacab2814efcf8952
user <user@user-PC>
2016-02-22 14:33:46 +0700

    Update userlib

--- engine/application/controllers/system/Log.php
+++ engine/application/controllers/system/Log.php
@@ -5,7 +5,9 @@ class Log extends Admin_Controller {
 
     function __construct() {
         parent::__construct();
-        
+        if (!$this->has_access($this->get_roleid_by_url())){
+            redirect('error/unauthorize');
+        }
         $this->data['page_title'] = 'System Log';
     }
     
