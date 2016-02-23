00cdb07ffcdb12afc00bebd790412f7181629bb8
marwansaleh <marwansaleh@172.18.1.229>
2016-02-23 11:26:53 +0700

    Add migration mtr_tertanggung / nasabah

---
+++ engine/application/migrations/025_add_mtr_tertanggung.php
@@ -0,0 +1,110 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of Migration_add_marketing_polis_broker
+ *
+ * @author marwansaleh
+ */
+class Migration_add_mtr_tertanggung extends MY_Migration {
+    protected $_table_name = 'ref_tertanggung';
+    protected $_primary_key = 'id';
+    protected $_index_keys = array('nama_lengkap','nama_depan');
+    protected $_fields = array(
+        'id'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'auto_increment' => TRUE
+        ),
+        'nama_lengkap'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 90,
+            'null' => FALSE
+        ),
+        'nama_depan'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 30,
+            'null' => FALSE
+        ),
+        'nama_tengah'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 30,
+            'null' => TRUE
+        ),
+        'nama_belakang'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 30,
+            'null' => FALSE
+        ),
+        'alamat'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 254,
+            'null' => TRUE
+        ),
+        'kota'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'default' => 0
+        ),
+        'propinsi'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'default' => 0
+        ),
+        'kode_pos'    => array (
+            'type'  => 'VACRHAR',
+            'constraint' => 7,
+            'null' => TRUE
+        ),
+        'telepon'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 11,
+            'null' => TRUE
+        ),
+        'mobile'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 14,
+            'null' => TRUE
+        ),
+        'fax'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 11,
+            'null' => TRUE
+        ),
+        'email'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 50,
+            'null' => TRUE
+        ),
+        'created'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'created_by'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'modified'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'modified_by'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        )
+    );
+    
+}
+
+/*
+ * filename : 022_marketing_polis_broker.php
+ * location : /application/migrations/022_marketing_polis_broker.php
+ */
