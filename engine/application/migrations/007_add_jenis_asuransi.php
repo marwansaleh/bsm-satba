cf5f0cbd94b6994a07434b270087b56be53cb877
marwansaleh <marwansaleh@172.18.1.229>
2016-02-23 12:13:39 +0700

    Update migration jenis_asuransi --> feeding data

--- engine/application/migrations/007_add_jenis_asuransi.php
+++ engine/application/migrations/007_add_jenis_asuransi.php
@@ -15,39 +15,14 @@ class Migration_add_jenis_asuransi extends MY_Migration {
             'unsigned' => TRUE,
             'auto_increment' => TRUE
         ),
-        'category'    => array(
+        'kategori'    => array(
             'type' => 'ENUM("general","eben")',
             'default' => 'general',
             'null' => FALSE
         ),
-        'is_permil'    => array(
-            'type' => 'TINYINT',
-            'constraint' => 1,
-            'default' => 0
-        ),
-        'fee_bri'    => array(
-            'type' => 'DECIMAL',
-            'constraint' => '6,4',
-            'default' => 0.0000
-        ),
-        'captive_piutang' => array(
-            'type'  => 'VARCHAR',
-            'constraint' => 15,
-            'null' => FALSE
-        ),
-        'captive_pendapatan' => array(
-            'type'  => 'VARCHAR',
-            'constraint' => 15,
-            'null' => FALSE
-        ),
-        'non_captive_piutang' => array(
-            'type'  => 'VARCHAR',
-            'constraint' => 15,
-            'null' => FALSE
-        ),
-        'non_captive_pendapatan' => array(
-            'type'  => 'VARCHAR',
-            'constraint' => 15,
+        'nama'    => array(
+            'type' => 'VARCHAR',
+            'constraint' => 30,
             'null' => FALSE
         ),
         'created'   => array(
@@ -76,6 +51,212 @@ class Migration_add_jenis_asuransi extends MY_Migration {
         )
     );
 
+    public function up(){
+        parent::up();
+        //Need seeding ?
+        $this->_seed(array(
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Fire',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Fire Special Risk/Consortium',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Marine Cargo',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Marine Hull',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Motor Vehicle',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'CIT/CIS/CICB',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'eben',
+                'nama'          => 'Personal Accident',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Engineering',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Public Liability',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Fidelity Guarantee',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Burglary Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Property All Risk',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Loss of Gross Rentall',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Business Interuption',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Earthquake',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Crops Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Comprehensive General Liability',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Aviation',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Billboard Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Terrorism & Sabotage',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Hole in One',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'eben',
+                'nama'          => 'Health Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Bonding',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Credit Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            ),
+            array(
+                'kategori'      => 'general',
+                'nama'          => 'Oil & Gas Insurance',
+                'created'       => time(),
+                'created_by'    => 1,
+                'modified'      => time(),
+                'modified_by'   => 1
+            )
+        ));
+    }
 }
 
 /*
