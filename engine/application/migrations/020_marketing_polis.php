64a5ba6d11d333129d89511c5711c1fb15c76edd
marwansaleh <marwansaleh@172.18.1.229>
2016-02-23 10:01:01 +0700

    Add new migration marketing_polis

---
+++ engine/application/migrations/020_marketing_polis.php
@@ -0,0 +1,157 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of Migration_add_marketing_polis
+ *
+ * @author marwansaleh
+ */
+class Migration_add_marketing_polis extends MY_Migration {
+    protected $_table_name = 'rel_marketing_polis';
+    protected $_primary_key = 'id';
+    protected $_index_keys = array('nomor_polis');
+    protected $_fields = array(
+        'id'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'auto_increment' => TRUE
+        ),
+        'sumber_bisnis' => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'default' => 1
+        ),
+        'nomor_polis'    => array (
+            'type'  => 'VARCHAR',
+            'constraint' => 15,
+            'null' => FALSE
+        ),
+        'tertanggung'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'null' => FALSE
+        ),
+        'okupasi'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'null' => FALSE
+        ),
+        'jenis_asuransi'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'null' => FALSE
+        ),
+        'periode_mulai'    => array (
+            'type'  => 'DATE',
+            'null' => FALSE
+        ),
+        'periode_akhir'    => array (
+            'type'  => 'DATE',
+            'null' => FALSE
+        ),
+        'mata_uang'    => array (
+            'type'  => 'INT',
+            'constraint' => 11,
+            'null' => FALSE
+        ),
+        'jumlah_pertanggungan' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00,
+            'null' => FALSE
+        ),
+        'jumlah_pertanggungan_idr' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00,
+            'null' => FALSE
+        ),
+        'suku_premi'    => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '6,3',
+            'default' => 0.000,
+            'null' => FALSE
+        ),
+        'tipe_premi' => array(
+            'type' => 'ENUM("prosen","promil")',
+            'default' => 'prosen',
+            'null' => FALSE
+        ),
+        'jumlah_premi' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00,
+            'null' => FALSE
+        ),
+        'jumlah_premi_idr' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00,
+            'null' => FALSE
+        ),
+        'biaya_lain' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '10,2',
+            'default' => 0.00
+        ),
+        'biaya_lain_idr' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '10,2',
+            'default' => 0.00
+        ),
+        'total_premi' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00
+        ),
+        'total_premi_idr' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00
+        ),
+        'persetujuan' => array(
+            'type' => 'ENUM("pusat","cabang")',
+            'default' => 'pusat'
+        ),
+        'sales' => array(
+            'type' => 'INT',
+            'constraint' => 11,
+            'null' => FALSE
+        ),
+        'komisi_kembali' => array (
+            'type'  => 'NUMERIC',
+            'constraint' => '15,2',
+            'default' => 0.00
+        ),
+        'created'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'created_by'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'modified'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        ),
+        'modified_by'   => array(
+            'type'  => 'INT',
+            'constraint' => 11,
+            'unsigned' => TRUE,
+            'default' => 0
+        )
+    );
+    
+}
+
+/*
+ * filename : 020_marketing_polis.php
+ * location : /application/migrations/020_marketing_polis.php
+ */
